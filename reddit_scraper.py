57e3d723868a6064b0c70158eb17679ced4208ad
Frank Tran <frank.tran@live.ca>
2021-04-17 00:56:06 -0400

    Made the scraper cooler

--- reddit_scraper.py
+++ reddit_scraper.py
@@ -1,18 +1,38 @@
-""" 
+"""
 TODO: navigate to subreddit
 TODO: iterate through each post filtering out the tags
 TODO: iterate through each post and filter out the comments for cashtags
-TODO: store the above in a table 
+TODO: store the above in a table
 TODO: sort by most mentioned
 TODO: visualize in a graph
-TODO: create a frontend table to display  
+TODO: create a frontend table to display
 """
-
-import praw
-import json
 import sqlite3
-import pandas as pd
+import json
+import praw
 from datetime import datetime
+import numpy as np
+import pandas as pd
+import yfinance as yf
+import logging
+
+# create logger
+logger = logging.getLogger('reddit_scrapper')
+logger.setLevel(logging.DEBUG)
+
+# create console handler and set level to debug
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+
+# create formatter
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+
+# add formatter to ch
+ch.setFormatter(formatter)
+
+# add ch to logger
+logger.addHandler(ch)
+
 
 reddit = praw.Reddit(
     client_id="CYi9axqx8plAYw",
@@ -34,39 +54,85 @@ class StockPost(object):
 
 
 class SubredditScraper:
-    # +wallstreetbets+stocks
 
-    def __init__(self):
-        self.posts = []
+    def __init__(self, searchRange):
+        self.exclusions = [
+            'ALL', 'AND', 'APPLE', 'ASK', 'BETA', 'BETS', 'BID', 'BIG', 
+            'BILL', 'BMW', 'CBD', 'CEO', 'CFO', 'CHEAP', 'CLOSE', 'COVID', 
+            'CURE', 'DFV', 'DOING', 'EDIT', 'EVERY', 'FAANG', 'FIX', 'FOMO', 
+            'FTD', 'FUCK', 'GAS', 'GOING', 'GOOD', 'GRANT', 'HOLY', 'ICON', 
+            'IMO', 'IOU', 'IPO', 'IRA', 'IRS', 'LARGE', 'LOOK', 'LOVE', 'MACD', 
+            'MEDIA', 'META', 'MEETS', 'MOLY', 'MONEY', 'MOON', 'MORE', 'NASA', 
+            'NEW', 'NEWS', 'NOT', 'ONCE', 'ONLY', 'PARKS', 'PENNY', 'PHIL', 
+            'POP', 'PRICE', 'PUTS', 'QUICK', 'REIT', 'ROPE', 'RSI', 'SAFE', 
+            'SAME', 'SEEK', 'SHARE', 'SHORT', 'STOCK', 'TAX', 'TDLR', 'THAN', 
+            'THE', 'THEME', 'THREE', 'TSX', 'USD', 'WHERE', 'WSB', 'YEAR', 'YOLO', 'YOUR'
+            ]
+
+        self.posts: StockPost(postID, postURL, postTitle, postText, upvotes, comments) = []
         self.tickers = dict()
-        subreddit = reddit.subreddit("pennystocks")
-
         # iterate through the posts and store them as an object
-        for submission in subreddit.top("day"):
-            post = StockPost(submission.id, submission.url, submission.title,
-                             submission.selftext, submission.score, submission.comments)
-            self.posts.append(post)
+        try:
+            logger.info("Gathering Reddit Posts from r/pennystocks, r/wallstreetbets, & r/stocks...")
+
+            subs = ["pennystocks", "wallstreetbets", "stocks"]
+            # Iterate through all posts of subreddits above for given range
+            for sub in subs:
+                for submission in reddit.subreddit(sub).top(searchRange):
+                    try:
+                        logger.info(str(submission.title))
+                        post = StockPost(str(submission.id), str(submission.url), str(submission.title),
+                                        str(submission.selftext), submission.score, submission.comments)
+                        self.posts.append(post)
+                    except Exception as e:
+                        logger.warning(e)
+                        continue
+                
+                logger.info("Number of Post Retrieved Today: " + str(len(self.posts)))
+
+        except Exception as e:
+            logger.error(e)
 
     # Looks for potential tickers from a string of text
     def tickerIdentifier(self, text):
         # retrieve cashtag and any capitalized words
-        potentialTickers = [
-            word for word in text.split() if word.isupper() or "$" in word]
+        try:
+        # retrieve cashtag and any capitalized words
+            potentialTickers = [
+                word for word in text.split() if word.isupper() or "$" in word]
         # remove any special characters
-        potentialTickers = [''.join(e for e in word if e.isalnum())
-                            for word in potentialTickers]
+            potentialTickers = [''.join(e for e in word if e.isalnum())
+                                for word in potentialTickers]
         # retrieve only 4 letter words
-        potentialTickers = [
-            word for word in potentialTickers if len(word) == 4]
+            potentialTickers = [
+                word for word in potentialTickers if len(word) >= 3 and len(word) <= 5 and word.isalpha()]
+        
+        # add to dictionary
+            for ticker in potentialTickers:
+                print(ticker)
+                if ticker not in self.exclusions:
+                    self.tickers[ticker] = self.tickers.get(ticker, 0) + 1  
 
-        # store them in a dictionary
-        print("Storing in Dict")
-        for ticker in potentialTickers:
-            print(ticker)
-            self.tickers[ticker] = self.tickers.get(ticker, 0) + 1
+            return self.tickers
 
-        print(json.dumps(self.tickers, indent=4))
+        except Exception as e:
+            logger.error(e)
 
+if __name__ == '__main__':
+    tickers = dict()
+    logger.info("Reddit Scraper Starting...")
+    scraper = SubredditScraper("day")
+    for redditPost in scraper.posts:
+        postTitleTickers = scraper.tickerIdentifier(redditPost.postTitle)
+        postTickers = scraper.tickerIdentifier(redditPost.postText)
+        
+        # merge dictionaries
+        temp = {**postTickers, **postTitleTickers}
+        tickers = {**tickers, **temp}
 
-s = SubredditScraper()
-s.tickerIdentifier(s.posts[0].postText)
+        # iterate through comments as well 
+        # for comment in redditPost.comments:
+        #     postComment = scraper.tickerIdentifier(comment.body)
+        #     tickers = {**tickers, **postComment}
+
+    print(json.dumps(tickers, indent=4))
